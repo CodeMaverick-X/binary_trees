eb0d93a5bdb8b212d3bfda13127adc54f6d8ba73
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-11-16 02:25:36 -0800

    Tree's depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_depth - calculate the depth of a tree
  * @tree: pointer to the root node
@@ -7,9 +6,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if ((tree && tree->parent))
+	if (tree && tree->parent)
 	{
-		return (1 + binary_tree_depth);
+		return (1 + binary_tree_depth(tree->parent));
 	}
+
 	return (0);
 }
+
