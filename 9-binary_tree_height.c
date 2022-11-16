9d1608340cd093c72939cf85464de1f0b4cddffb
reinhard <reinhard2057@gmail.com>
2022-11-16 10:05:29 +0100

    BUG FIX: return 0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,17 +12,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int height_r = 0;
 	size_t max = 0;
 
+
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 
 	height_l = binary_tree_height(tree->left);
 	height_r = binary_tree_height(tree->right);
 
+	if (tree->left == NULL && tree->right == NULL)
+		max = -1;
 	if (height_l >= height_r)
-		max = height_l + 1;
+		max += height_l + 1;
 	else
-		max = height_r + 1;
+		max += height_r + 1;
 
 
 	return (max);
