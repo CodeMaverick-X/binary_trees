cc5323c28f990a8fb6e6b917b3265badbe2cb1ce
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-11-17 11:48:33 -0800

    Working ⚙️ ⚙️ ⚙️ ⚙️ 🧲

--- binary_trees.h
+++ binary_trees.h
@@ -29,7 +29,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 /* PROTOTYPES */
-
+int binary_tree_is_complete(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
