dda12db1ec142929e231452b3a771a875aff06d7
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-11-17 10:59:54 -0800

    Task 19

--- binary_trees.h
+++ binary_trees.h
@@ -59,5 +59,5 @@ avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *array_to_heap(int *array, size_t size);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 #endif /* _BINARY_TREES_H_ */
